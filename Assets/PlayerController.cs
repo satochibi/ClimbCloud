7995becb0ccf56db9b21060189d3f973d71be5e7
satochibi <satochibi0120@gmail.com>
2016-09-21 23:08:18 +0900

    スマートフォンの操作に最適化

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour {
 	float jumpForce = 680.0f;
 	float walkForce = 30.0f;
 	float maxWalkSpeed = 2.0f;
+	float threshold = 0.2f;
 
 	// Use this for initialization
 	void Start () {
@@ -27,15 +28,15 @@ public class PlayerController : MonoBehaviour {
 
 
 		//jump
-		if (Input.GetKeyDown(KeyCode.Space) && (this.rigid2D.velocity.y == 0))
+		if (Input.GetMouseButtonDown(0) && (this.rigid2D.velocity.y == 0))
 		{
 			this.rigid2D.AddForce(transform.up * this.jumpForce);
 		}
 
 		//左右移動
 		int key = 0;
-		if (Input.GetKey(KeyCode.RightArrow)) key = 1;
-		if (Input.GetKey(KeyCode.LeftArrow)) key = -1;
+		if (Input.acceleration.x > this.threshold) key = 1;
+		if (Input.acceleration.x < -this.threshold) key = -1;
 
 		float speedx = Mathf.Abs(this.rigid2D.velocity.x);
 
